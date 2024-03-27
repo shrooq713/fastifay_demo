43900e8b13d272d65d679a8d77f85b418fce3836
Shrooq <42460072+shrooq713@users.noreply.github.com>
2024-03-27 05:09:05 +0300

    add respons schema and parameter

--- src/user-controller.js
+++ src/user-controller.js
@@ -1,8 +1,23 @@
+const responseSchema = {
+    response: {
+        200: {
+            properties: {
+                message: { type: 'string' },
+            },
+        }
+    }
 
+};
 const userController = (fastify, options, done) => {
-    fastify.get('/',(req, reply) => {
+    fastify.get('/', {schema: responseSchema},(req, reply) => {
         return {
             message: 'Hello World!!'
+        };
+    });
+
+    fastify.get('/hello/:name', {},(req, reply) => {
+        return {
+            message: `Hello ${req.parameter.name}!`
         };
     });
     done();
